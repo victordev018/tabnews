979fb71ca07762636d62a4d8f55e39ee97f939da
Joao Victor <133924640+victordev018@users.noreply.github.com>
2026-08-14 01:16:49 +0000

    fix `prettier` style lint

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ function Home() {
   return <h1>Faça sua parte e confie no processo, jota.</h1>;
 }
 
-function teste(){
-    console.log('teste')
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
